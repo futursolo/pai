406a8073565af0b561b097319cffca93956f7016
Kaede Hoshikawa <11693215+futursolo@users.noreply.github.com>
2025-08-20 21:04:07 +0900

    fix: reduce gibberish in ipex repo

--- common/scripts/apt-add-intel-repo.sh
+++ common/scripts/apt-add-intel-repo.sh
@@ -8,12 +8,12 @@ mkdir --parents --mode=0755 /usr/share/keyrings
 
 # OneAPI
 curl -fsSL https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB | \
-  gpg --dearmor | tee /usr/share/keyrings/intel-oneapi-archive-keyring.gpg
+  gpg --dearmor > /usr/share/keyrings/intel-oneapi-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/intel-oneapi-archive-keyring.gpg] https://apt.repos.intel.com/oneapi all main " \
     > /etc/apt/sources.list.d/oneapi.list
 
 # Intel Graphics Driver
 curl -fsSL https://repositories.intel.com/gpu/intel-graphics.key | \
-    gpg --dearmor | tee /usr/share/keyrings/intel-graphics-archive-keyring.gpg
+    gpg --dearmor > /usr/share/keyrings/intel-graphics-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/intel-graphics-archive-keyring.gpg arch=amd64] https://repositories.intel.com/gpu/ubuntu noble unified" \
     > /etc/apt/sources.list.d/intel-graphics.list
